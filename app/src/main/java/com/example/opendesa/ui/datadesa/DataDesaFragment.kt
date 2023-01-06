753da2240d90f14a6860cd12df791f765a29f1d4
zhafranP <zhafrangom12@gmail.com>
2023-01-06 07:54:49 +0700

    connect to db

--- app/src/main/java/com/example/opendesa/ui/datadesa/DataDesaFragment.kt
+++ app/src/main/java/com/example/opendesa/ui/datadesa/DataDesaFragment.kt
@@ -1,12 +1,12 @@
 package com.example.opendesa.ui.datadesa
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.opendesa.R
@@ -19,8 +19,9 @@ class DataDesaFragment : Fragment()
 
     private lateinit var adapter: DataDesaAdapter
     private lateinit var recyclerView: RecyclerView
+    private lateinit var dataDesa : List<Desa>
 
-    private var listDesa: MutableList<Desa> = mutableListOf()
+    private val viewModel: DataDesaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,27 +39,23 @@ class DataDesaFragment : Fragment()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initData()
+
 
         val layoutManager = LinearLayoutManager(context)
-        recyclerView = view.findViewById(R.id.recyclerview_desa)
-        recyclerView.layoutManager = layoutManager
-        recyclerView.setHasFixedSize(true)
-        adapter = DataDesaAdapter(listDesa, activity)
-        recyclerView.adapter = adapter
+        viewModel.dataDesa.observe(this.viewLifecycleOwner){
+            dataDesa = it
+            Log.d("Err", dataDesa.toString())
+            recyclerView = view.findViewById(R.id.recyclerview_desa)
+            recyclerView.layoutManager = layoutManager
+            recyclerView.setHasFixedSize(true)
+            adapter = DataDesaAdapter(dataDesa, activity)
+            recyclerView.adapter = adapter
+
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    private fun initData() {
-        val desa = resources.getStringArray(R.array.array_desa)
-        listDesa.clear()
-
-        for (i in desa.indices) {
-            listDesa.add(Desa(desa[i]))
-        }
     }
 }
--- app/src/main/java/com/example/opendesa/ui/datadesa/Desa.kt
+++ app/src/main/java/com/example/opendesa/ui/datadesa/Desa.kt
@@ -1,3 +1,9 @@
 package com.example.opendesa.ui.datadesa
 
-data class Desa (val name: String?)
+import com.squareup.moshi.Json
+
+data class Desa (val id: String,
+                 @Json(name = "kode_desa") val kodeDesa : String,
+                 @Json(name = "nama_desa") val namaDesa : String,
+                 val website : String,
+                 @Json(name = "luas_wilayah") val luasWilayah : Double)
